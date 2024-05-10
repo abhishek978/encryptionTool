5ecf6191131b075cb8609c73d4fb48a4f08f5b32
abhi2001 <abhishek.upadhyay@antiersolutions.com>
2024-05-10 15:24:33 +0530

    updated code

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const ejs = require("ejs");
 const { encryptPrivateKey, decryptPrivateKey } = require("./helper");
 
 const app = express();
-const port = 3000;
+// const port = 3000;
 
 // Set EJS as the view engine
 app.set("view engine", "ejs");
